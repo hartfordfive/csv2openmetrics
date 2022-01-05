732e8307472261434a447e082b28a9cefdb95a7f
Alain Lefebvre <hartfordfive@gmail.com>
2022-01-05 18:37:03 -0500

    Adding in code

--- generator/metric.go
+++ generator/metric.go
@@ -5,14 +5,17 @@ import (
 	"math"
 	"strconv"
 	"strings"
+
+	"github.com/araddon/dateparse"
 )
 
 type Metric struct {
-	Name   string
-	Labels map[string]string
-	Value  float64
-	Type   string
-	Help   string
+	Name      string
+	Timestamp string
+	Labels    map[string]string
+	Value     float64
+	Type      string
+	Help      string
 }
 
 // ValueCanBeInt is used to determine if the float value can really be represented as an int
@@ -28,15 +31,22 @@ func convertToIntString(val float64) string {
 }
 
 func (m *Metric) ToString() string {
+
+	fmt.Println("Date passed in:", m.Timestamp)
+	ts, err := dateparse.ParseLocal(m.Timestamp)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	if len(m.Labels) >= 1 {
 		flattendLabels := []string{}
 		for k, v := range m.Labels {
 			flattendLabels = append(flattendLabels, fmt.Sprintf("%s=\"%s\"", k, v))
 		}
 		if ValueCanBeInt(m.Value) {
-			return fmt.Sprintf("%s{%s} %s", m.Name, strings.Join(flattendLabels, ", "), convertToIntString(m.Value))
+			return fmt.Sprintf("%s{%s} %s %s", m.Name, strings.Join(flattendLabels, ", "), convertToIntString(m.Value), ts)
 		} else {
-			return fmt.Sprintf("%s{%s} %f", m.Name, strings.Join(flattendLabels, ", "), m.Value)
+			return fmt.Sprintf("%s{%s} %f %s", m.Name, strings.Join(flattendLabels, ", "), m.Value, ts)
 		}
 
 	} else {
